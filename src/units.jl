f4474bf27d56114dc9118b17dd9c616e03eccd48
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-09-17 15:03:18 +0100

    Dimensionals are rational. Period.
    Use to be any number under the sun. Now must be a Rational{Int64}.
    We use Int64 rather than Int8 because otherwise, we must make sure any
    manipulation of Dimension object is done using Int8 (rather than a more
    standard integer). Could be done, but painfull.

--- src/units.jl
+++ src/units.jl
@@ -1,20 +1,20 @@
 """ Dimensionlity descriptor """
-immutable Dimensions{T <: Real} <: FixedVectorNoTuple{9, T}
-    length::T
-    mass::T
-    time::T
-    current::T
-    temperature::T
-    quantity::T
-    luminosity::T
-    angle::T
-    solid_angle::T
+immutable Dimensions <: FixedVectorNoTuple{9, Rational{Int64}}
+    length::Rational{Int64}
+    mass::Rational{Int64}
+    time::Rational{Int64}
+    current::Rational{Int64}
+    temperature::Rational{Int64}
+    quantity::Rational{Int64}
+    luminosity::Rational{Int64}
+    angle::Rational{Int64}
+    solid_angle::Rational{Int64}
 end
-function Dimensions{T <: Real}(;length::T=0, mass::T=0, time::T=0,
-    current::T=0, temperature::T=0, quantity::T=0,
-    luminosity::T=0, angle::T=0, solid_angle::T=0)
-    Dimensions{T}(length, mass, time, current, temperature, quantity,
-    luminosity, angle, solid_angle)
+function Dimensions(;length::Real=0, mass::Real=0, time::Real=0,
+    current::Real=0, temperature::Real=0, quantity::Real=0,
+    luminosity::Real=0, angle::Real=0, solid_angle::Real=0)
+    Dimensions(length, mass, time, current, temperature, quantity,
+        luminosity, angle, solid_angle)
 end
 
 """ Abstract unit dimension """
@@ -54,22 +54,30 @@ superscript(i) = map(repr(i)) do c
     c   ==  '8' ? '\u2078' :
     c   ==  '9' ? '\u2079' :
     c   ==  '0' ? '\u2070' :
-    error("Unexpected Chatacter")
+    c   ==  '/' ? '\u2032' :
+    c   ==  '.' ? '\u2027' :
+    error("Unexpected Character $c")
 end
 
 function unit_symbol(unit::AbstractUnit)
     sum(dimensionality(unit)) == 1 &&
         return "$(unit_system(unit)): $(dimensionality(unit))"
     const system = unit_system(unit)
-    const T = eltype(dimensionality(unit))
     const iters = 1:length(dimensionality(unit)), dimensionality(unit)
     const nonzero = filter(zip(iters...)) do dim
         dim[2] != 0
     end
     const strings = map(nonzero) do dim
-        const args = vcat(zeros(T, dim[1] - 1), T[1], zeros(T, 9 - dim[1]))
-        const pure = Unit{system, Dimensions{T}(args...)}()
-        "$(unit_symbol(pure))" * (dim[2] == 1 ? "": superscript(dim[2]))
+        const args = vcat(
+            zeros(Int16, dim[1] - 1), Int16[1], zeros(Int16, 9 - dim[1]))
+        const pure = Unit{system, Dimensions(args...)}()
+        if dim[2] == 0
+            "$(unit_symbol(pure))"
+        elseif dim[2].den == 1
+            "$(unit_symbol(pure))" * superscript(dim[2].num)
+        else
+            "$(unit_symbol(pure))" * superscript(dim[2])
+        end
     end
     join(strings, "⋅")
 end
@@ -83,20 +91,20 @@ function *(a::Unit, b::Unit)
     const dims = dimensionality(a) + dimensionality(b)
     all(dims .== 0) && return 1
     const system = prefer_system(a, b)
-    Unit{system, Dimensions(dimensionality(a) + dimensionality(b))}()
+    Unit{system, dimensionality(a) + dimensionality(b)}()
 end
 
 function /(a::Unit, b::Unit)
     const dims = dimensionality(a) - dimensionality(b)
     all(dims .== 0) && return 1
     const system = prefer_system(a, b)
-    Unit{system, Dimensions(dimensionality(a) - dimensionality(b))}()
+    Unit{system, dimensionality(a) - dimensionality(b)}()
 end
 function ^(a::Unit, p::Integer)
     p == 0 && return 1
     Unit{unit_system(a), dimensionality(a) * p}()
 end
-function ^(a::Unit, p::Real)
+function ^(a::Unit, p::Rational)
     abs(p) < 1e-12 && return 1
     Unit{unit_system(a), dimensionality(a) * p}()
 end
@@ -116,10 +124,14 @@ function conversion_factors(a::Unit, b::Unit)
                 dims[2] == 0 && return 1
                 args = zeros(Int64, 9)
                 args[dims[1]] = 1
-                const dimensions = Dimensions{Int64}(args...)
+                const dimensions = Dimensions(args...)
                 const Da = Unit{unit_system(a), dimensions}()
                 const Db = Unit{:SI, dimensions}()
-                conversion_factors(Da, Db)[1]^dims[2]
+                if dims[2].den == 1
+                    conversion_factors(Da, Db)[1]^dims[2].num
+                else
+                    conversion_factors(Da, Db)[1]^dims[2]
+                end
         end, 0
     elseif unit_system(a) == :SI
         invert_conversion(conversion_factors(b, a)...)
@@ -137,5 +149,5 @@ function conversion_factors(a::Unit, b::Unit)
 end
 
 function Base.show(io::IO, unit::Unit)
-    print(io, "$(unit_system(unit))($(unit_symbol(unit)))")
+    print(io, "$(unit_symbol(unit))[$(unit_system(unit))]")
 end
